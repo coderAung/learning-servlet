1c9b38bd4d25dcf361ad2b424b051b958d60b0f4
coderAung <yewontaung798@gmail.com>
2024-06-27 10:49:04 +0630

    saving staffs

--- blog/src/main/java/org/core/blog/controller/AbstractController.java
+++ blog/src/main/java/org/core/blog/controller/AbstractController.java
@@ -1,7 +1,10 @@
 package org.core.blog.controller;
 
+import java.io.IOException;
+
 import org.apache.tomcat.jdbc.pool.DataSource;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,7 +14,10 @@ public abstract class AbstractController extends HttpServlet {
     
 	protected DataSource dataSource;
 
-    protected void view(HttpServletRequest request, HttpServletResponse response, String logicalViewName) {
+    protected void view(HttpServletRequest request, HttpServletResponse response, String logicalViewName) throws ServletException, IOException {
+    	getServletContext()
+    	.getRequestDispatcher("/pages/%s.jsp".formatted(logicalViewName))
+    	.forward(request, response);
     }
 
     protected void redirect(HttpServletRequest request, HttpServletResponse response, String path) {
