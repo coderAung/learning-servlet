2da7c3b8e5e965aff992be05f24fabfbf1311654
coderAung <yewontaung798@gmail.com>
2024-07-03 18:04:35 +0630

    created an abstract controller

---
+++ blog/src/main/java/org/core/blog/controller/AbstractController.java
@@ -0,0 +1,15 @@
+package org.core.blog.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AbstractController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void view(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+		
+}
